b5fa7293578c6b265ce1ba255f91052d8d2c7cf4
PAL CHAKRABORTY <pal.chakraborty@ericsson.com>
2017-10-14 12:51:21 +0530

    Java first program

---
+++ DataType (2).java
@@ -0,0 +1,10 @@
+
+public class DataType {
+	static int i;
+	public static void main(String[] args) {
+       int j;
+       
+       System.out.println("i:" + i);
+       System.out.println("i:" + j);
+	}
+}
