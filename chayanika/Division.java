344518e50f3d4998fe720ed457d04a8ebc527df8
chayanika <chayanika>
2017-10-22 22:37:07 +0530

    message you want to give

---
+++ chayanika/Division.java
@@ -0,0 +1,12 @@
+public class Division {
+   public static void main(String args[]){
+		String s1=args[0];
+		String s2=args[1];
+		int n1=Integer.parseInt(s1);
+		int n2=Integer.parseInt(s2);
+		
+		  System.out.println("Sum = "+(n1/n2));
+		   
+		                                    
+		}
+    }
--- chayanika/first.java
+++ chayanika/first.java
@@ -1,4 +1,4 @@
-public class Second {
+public class first {
 	public static void main (String []args) {
 		System.out.println("Hello");
 		System.out.println("Chayanika Roy");
