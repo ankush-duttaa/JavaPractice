a66f8bd3f2be3e30f722e4d003acfdc2d1e82b0e
chayanika <chayanika>
2017-10-22 23:32:23 +0530

    message you want to give

---
+++ chayanika/Mul.java
@@ -0,0 +1,10 @@
+public class Mul {
+   public static void main(String args[]){
+	   String s=args[0];
+		int a=Integer.parseInt (s);
+		for(int i=1;i<=10;i++){
+		System.out.println(""+a+" * "+i+" = " +(i*a));
+            }
+		                                    
+		}
+    }
